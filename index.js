3941227228e6f06bb0a16097681f3eda3b07abe7
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-15 02:08:49 -0500

    feat: route pages

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import http from "http";
+import fs from "fs";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+const server = http.createServer();
+server.on("request", (req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("index.html"));
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./about.html"));
+  } else if (req.url === "/contact-me") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./contact-me.html"));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(fs.readFileSync("./404.html"));
+  }
 });
 server.listen(8080);
