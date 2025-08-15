55dfd389c066d374733119913049bc0b7e7eb58c
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-14 22:49:34 -0500

    feat: create basic server

---
+++ index.js
@@ -0,0 +1,11 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+server.listen(8080);
