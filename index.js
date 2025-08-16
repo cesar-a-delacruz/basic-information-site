21709a9f29d0226359edb1631db24bddf5cca788
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-16 18:16:55 -0500

    refactor: turn pages into views with ejs

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ import path from "path";
 
 const app = express();
 
+app.set("views", path.join(path.dirname('.'), "views"));
+app.set("view engine", "ejs");
+
 app.listen(8080, (error) => {
   if (error) throw error;
   console.log("running...");
 });
 
-const options = { root: path.dirname(".") };
-
 app.get("/", (req, res) => {
-  res.status(200).sendFile("./index.html", options);
+  res.status(200).render("index", { title: "Home - My Info Site" });
 });
 app.get("/about", (req, res) => {
-  res.status(200).sendFile("./about.html", options);
+  res.status(200).render("about", { title: "About Me" });
 });
 app.get("/contact-me", (req, res) => {
-  res.status(200).sendFile("./contact-me.html", options);
+  res.status(200).render("contact-me", { title: "Contact Me - My Info Site" });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./404.html", options);
+  res.status(404).render("contact-me", { title: "404 - Page Not Found" });
 });
-
